d1087891b39fd26b70fa5816766f0fc5ce9d4898
Thuvashika <shika.pviji@gmail.com>
2022-10-07 15:25:41 +0530

    New commit

---
+++ src/com/learn/java/self/GreatGreat.java
@@ -0,0 +1,18 @@
+package com.learn.java.self;
+
+public class GreatGreat {
+	public static int a=10;
+	static{
+		System.out.println("yes");
+	}
+	public void print() {
+		System.out.println("GreatGreat print");
+	}
+	public static void display() {
+		System.out.println("GreatGreat display");
+	}
+	public enum Days {
+		Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday;
+	}
+
+}
